88989d7fe7fb0498a838dc9d129b7df978eae72d
Ales Lerch <ales.lerch@gmail.com>
2015-08-19 01:30:55 +0200

    Improved main function

--- itun.py
+++ itun.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python 
+#!/usr/local/bin/python
 
 import shutil
 import sys
@@ -9,7 +9,7 @@ myDir = "/Volumes/Macintosh\ SD/Movies/iTunes\ Movies/"
 
 # procedure mv fileA to directory as fileB
 def mv(fileA,fileB):
- if os.path.exits(fileA): 
+ if os.path.exits(fileA):
    shutil.move(fileA,fileB)
  else:
    print "File not found!"
@@ -24,7 +24,7 @@ def checkFile(fil):
 # procedure ln to create symbolic link
 def ln(src,dst):
   if os.path.isdir(src) and os.path.isdir(dst):
-  os.symlink(src,dst)
+      os.symlink(src,dst)
  else:
   print "Link is broken"
   sys.exit(0)
@@ -32,20 +32,26 @@ def ln(src,dst):
 # main code
 
 if len(sys.arg) >= 1:
-  soubor = sys.argv[1]
-else:
-   print "File not found"
-   sys.exit(0)
-  #print "File name not found Exit? Y|n"
-  #soubor = raw_input('Enter file name')
-
-if len(sys.argv) >= 2:
+    if type(sys.argv[1]) is string:
+        soubor = sys.argv[1]
+    else:
+        print "File not found"
+        sys.exit(0)
+        #print "File name not found Exit? Y|n"
+        #soubor = raw_input('Enter file name')
+elif len(sys.argv) >= 2:
   dirr = sys.argv[2]
-
+elif len(sys.argv) >= 3:
+    soubor = sys.argv[1]
+    dirr = sys.argv[2]
+    if sys.argv[3] == "-d":
+        #spcialni volba
+else:
+    #prit else statment
 #............................
 
 # other code - maintance need
- """ if soubor.endswith(".mp4"):
+ ''' if soubor.endswith(".mp4"):
   if len(sys.arg) > 1:
    destination = sys.arg[2]
    if (os.path.isdir(destination)):
@@ -55,10 +61,7 @@ if len(sys.argv) >= 2:
      sys.exit(0)
   else:
     mv(soubor,destination)
- else:  
+ else:
   print"File is not mp4 format"
   sys.exit(0)
-"""
-
-
-
+'''
