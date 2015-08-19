dbd16345f24364d500f0aa605baa6f550e15600a
Ales Lerch <ales.lerch@gmail.com>
2015-08-19 17:17:40 +0200

    improvements to itun.py

--- itun.py
+++ itun.py
@@ -2,21 +2,31 @@
 
 import shutil
 import sys
+import optparse
 
 # default paths ...
 movieDir = "~/Music/iTunes/iTunes\ Media/Movies/"
 myDir = "/Volumes/Macintosh\ SD/Movies/iTunes\ Movies/"
+parser = optparse.OptionParser()
+parser.addOption('-d','--destination',dest='destination', help='Place to save file')
+
+(options,args) = parser.parser_args()
+
+if options.destination is None:
+    options.destination = myDir
+
+sayWhat = 'File, ',
 
 # procedure mv fileA to directory as fileB
-def mv(fileA,fileB):
- if os.path.exits(fileA):
-   shutil.move(fileA,fileB)
+def mv(pathFile,pathTo):
+ if os.path.exits(pathFile):
+   shutil.move(pathFile,pathTo)
  else:
    print "File not found!"
    sys.exit(0)
 
-def checkFile(fil):
-  if fil.endswith(".mp4"):
+def checkFile(_file):
+  if _file.endswith(".mp4"):
 	return True
   else:
 	return False
@@ -31,16 +41,16 @@ def ln(src,dst):
 
 # main code
 
- if soubor.endswith(".mp4"):
-  if len(sys.arg) > 1:
-   destination = sys.arg[2]
-   if (os.path.isdir(destination)):
-     mv(soubor,destination)
-   else:
-     print "Directory dosnt exists"
-     sys.exit(0)
-  else:
-    mv(soubor,destination)
- else:
+ if checkFile(sys.argv[2]):
+     if len(sys.arg) > 1:
+         destination = sys.arg[2]
+         if (os.path.isdir(destination)):
+            mv(soubor,destination)
+         else:
+             print "Directory dosnt exists"
+             sys.exit(0)
+     else:
+         mv(soubor,destination)
+else:
   print"File is not mp4 format"
   sys.exit(0)
