6fa63af713489f47eaef9969cf859259513dd9f7
Ales Lerch <ales.lerch@gmail.com>
2015-08-19 17:30:51 +0200

    improved and added defs

--- itun.py
+++ itun.py
@@ -15,40 +15,40 @@ parser.addOption('-d','--destination',dest='destination', help='Place to save fi
 if options.destination is None:
     options.destination = myDir
 
-sayWhat = 'File, ',
+sayWhat = 'file will be saved to ' + myDir
 
-# procedure mv fileA to directory as fileB
+# function list
 def mv(pathFile,pathTo):
- if os.path.exits(pathFile):
-   shutil.move(pathFile,pathTo)
- else:
-   print "File not found!"
-   sys.exit(0)
+    if os.path.exits(pathFile):
+        shutil.move(pathFile,pathTo)
+    else:
+        print "File not found!"
+        sys.exit(0)
 
 def checkFile(_file):
-  if _file.endswith(".mp4"):
-	return True
-  else:
-	return False
+    if _file.endswith(".mp4"):
+	       return True
+       else:
+           return False
 
-# procedure ln to create symbolic link
+def checkDir(_dir):
+    if (os.path.isdir(_dir)):
+        return True
+    else:
+        return False
+
 def ln(src,dst):
-  if os.path.isdir(src) and os.path.isdir(dst):
-      os.symlink(src,dst)
- else:
-  print "Link is broken"
-  sys.exit(0)
+    if os.path.isdir(src) and os.path.isdir(dst):
+        os.symlink(src,dst)
+    else:
+        print "Link is broken"
+        sys.exit(0)
 
 # main code
 
- if checkFile(sys.argv[2]):
-     if len(sys.arg) > 1:
-         destination = sys.arg[2]
-         if (os.path.isdir(destination)):
-            mv(soubor,destination)
-         else:
-             print "Directory dosnt exists"
-             sys.exit(0)
+ if len(sys.arg[1:]) >= 2 or len(sys.argv[1:]) >= 1:
+     if checkFile(sys.argv[2]):
+         mv(soubor,destination)
      else:
          mv(soubor,destination)
 else:
