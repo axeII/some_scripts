f61466353a0efacac2a01323f0e7b248f52831e6
Ales Lerch <ales.lerch@gmail.com>
2015-11-10 13:00:14 +0100

    map function

--- comic.py
+++ comic.py
@@ -12,53 +12,75 @@ def testFile(file_):
 	else:
 		return False
 
-def seraching(directory):
-	list_ = []
-	for dir_ in directory:
-		if testFile(dir_):
-			list_.add(dir_)	 
+def serachFile(directory,listX):
+	files = os.listdir(directory)
+	for dir_ in files:
+		if os.path.isdir(dir_):
+			serachFile(dir_,list_)
 		else:
-			seraching(dir_)
-	return list_
+			for d in directory:
+				print d 
+				#newList = []
+				#if testFile(d):
+					#newList.append(d)	
+				#else:
+				#	print "File %s is not compatible..." % (dir_)
+			list_.append(newList)
 
-def archive(list_):
+def printFile(fileX,nameX):
+	print "Adding %s to %s " % (fileX,nameX)
+
+def archive(image):
+	return os.path.abspath(image)
+
+def zipArchive(name,folder,list_):
+	#f = zipfile.ZipFile("./" + name,'w')
+	finalList = map(archive,list_)
+	for fin in finalList:
+		printFile(fin,name)
+	#	f.write(fin)
+	#f.close()
+
+def comicMode():
+	print "Entering comic mode..."
+	what = True
+	volume = 0
+	print "Current Volume is " + str(volume) + " what is your order?"
+	while what:
+		order = raw_input('Next[N],Previos[P],Status[S],Reset[R],End[E]: ')
+		if order == "N" or order == "n":
+			volume += 1
+			os.system("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume))
+		elif order == "P" or order == "p":
+			volume -= 1
+			os.system("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume))
+		elif order == "S" or order == "s":
+			print "Your current volume is " + str(volume)
+			os.system("ls -lh")
+		elif order == "E" or order == "e":
+			what = False
+		elif order == "R" or order == "r":
+			print "Reseting from %s to 0" % (volume)
+			volume = 0
+		else:
+			print "Wrong input.."
+
 	
 # Coping image (1 of 100) to filenam1.cbr 	
 # Coping image (22 of 67) to filenam2.cbr
 
 if __name__ == "__main__":
-	if sys.argv[1] == "mode":
-		print "Entering comic mode..."
-		what = True
-		volume = 0
-		print "Current Volume is " + str(volume) + " what is your order?"
-		while what:
-			order = raw_input('Next[N],Previos[P],Status[S],Reset[R],End[E]: ')
-			if order == "N" or order == "n":
-				volume += 1
-				os.system("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume))
-			elif order == "P" or order == "p":
-				volume -= 1
-				os.system("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume))
-			elif order == "S" or order == "s":
-				print "Your current volume is " + str(volume)
-				os.system("ls -lh")
-			elif order == "E" or order == "e":
-				what = False
-			elif order == "R" or order == "r":
-				print "Reseting from %s to 0" % (volume)
-				volume = 0
-			else:
-				print "Wrong input.."
+	if len(sys.argv[1:]) < 1:
+		print "navod"
 	else:
-		folders = sys.argv[1:]
-		for folder in folders:
-			files = os.listdir(folder)
-			name = os.path.basename(folder) + ".cbr"
-			f = zipfile.ZipFile("./" + name,"w")
-			for file_ in files:
-				fileName = os.path.abspath(folder) + "/" + file_
-				print "Adding %s  to  %s" % (fileName, name)
-				f.write(fileName)
-		f.close()
+		if sys.argv[1] == "mode":
+			comicMode()
+		else:
+			folders = sys.argv[1:]
+			for folder in folders:
+				list_ = []
+				serachFile(folder,list_)
+				name = os.path.basename(folder) + ".cbr"
+				print name, folder,list_
+				#zipArchive(name,folder,list_)
 
