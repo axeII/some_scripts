b690e06e1e7bdc80d94fe5db740c087378b46f8b
Ales Lerch <ales.lerch@gmail.com>
2016-07-26 15:41:42 +0200

    updating wifi.py

--- wifi.py
+++ wifi.py
@@ -1,63 +1,61 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 
-import os
-import sys
+import os,sys 
+import subprocess
 import getpass
+import optparse
 
-def is_connected(keyword,tempfile):
-	os.system("airport -I > " + tempfile)
-	f = open(tempfile,'r')
-	data = f.read()
-	f.close()
+def getParser():
+    parser = optparse.OptionParser()
+    #parser.add_option()
+    return parser.parse_args()
+
+def is_connected(keyword):
+	data = subprocess.check_output("airport -I")
 	return keyword in data
 
-def exists(keyword,tempfile):
-	os.system("airport -s > " + tempfile)
-	f = open(tempfile,'r')
-	data = f.read()
-	f.close()
+def exists(keyword):
+	data = subprocess.check_output("airport -s")
 	return keyword in data	
 
 def connect(name,device,password):
-	if password != "":
+	if password:
 		command = "networksetup -setairportnetwork %s %s %s" % (device,name,password)
-		print "Connecting to %s network..." % (name)
-		os.system(command)
-
+		print "Connecting to %s network..." % name
+                subprocess.call(command,shell=True)
 
 def turn(action,device):
 	command = "networksetup -setairportpower %s %s" % (device,action)
-	os.system(command)
-	print "Turning wifi " + action
+	print "Turning wifi " + str(action)
+        subprocess.call(command,shell=True)
 
-def doAction(input_,network):
-	device = "en0"
-	if input_ == "on" or input_ == "off":
-		turn(input_,device)
-	elif input_ == "log":
-		if exists(network,"/Users/Ales/Library/WifiLog/airport-scan.txt"):
+def doAction(device,args):
+    if args and len(args) < 4:
+	if 'on' in args or 'off' in args:
+            turn(args[0],device)
+	elif 'log' in args:
+		if exists(args[1]):
 			password = getpass.getpass("Password: ")	
-			connect(network,device,password)
-			if is_connected(network,"/Users/Ales/Library/WifiLog/connectino-info.txt"):
+			connect(args[1],device,password)
+			if is_connected(network):
 				print "Connection Success..."
 		else:
 			print "Network not found..."	
-	elif input_ == "logout":
-		os.system("sudo airport -z")
-	else:
-		print "Error input..."
+	elif 'logout' in args:
+	    subprocess.call("sudo airport -z", shell=True)
+	elif 'scan' in args:
+		print "\t List of all wifi spots: "
+		output = subprocess.check_output("airport -s",shell=True) 
+                print output
+    else:
+        print "Error input..."
 		
 if __name__ == "__main__":
-
-	if len(sys.argv) > 1:
-		cliAct = sys.argv[1]
-		if len(sys.argv) == 2:
-			doAction(cliAct,"")
-		elif len(sys.argv) == 3:
-			net = sys.argv[2]
-			doAction(cliAct,net)
-		else:
-			print "Ups too many arguments"
+        (option,args) = getParser()
+	if args:
+                if len(args) > 3:
+                    print "Ups too many arguments"
+                doAction('en0',args)
 	else:
-		print "wifi on | off | log"
+		print "$ wifi - on | off | log | logout | scan"
 
