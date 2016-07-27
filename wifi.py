dea2d51eb8a03546675465a4bf6f25a03b7466d2
Ales Lerch <ales.lerch@gmail.com>
2016-07-28 01:54:50 +0200

    options added, new scan priting, main edited

--- wifi.py
+++ wifi.py
@@ -7,9 +7,9 @@ import optparse
 
 def getParser(usage):
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('-c','--connect',action='optConnect',help='Which wifi you want to connect')
-    parser.add_option('-l','--logout',action='optLogout',help='logout from wifi network')
-    parser.add_option('-s','--scan',action='optScan',help='scan local network area')
+    parser.add_option('-c','--connect',dest='optConnect',help='Which wifi you want to connect')
+    parser.add_option('-l','--logout',dest='optLogout',help='logout from wifi network',action='store_true')
+    parser.add_option('-s','--scan',dest='optScan',help='scan local network area',action='store_true')
     return parser.parse_args()
 
 def is_connected(keyword):
@@ -30,14 +30,40 @@ def turn(action,device):
     subprocess.call(command,shell=True)
 
 def betterPrint(data):
+
+    def deSerialize(arrRepresentation):
+        arrRepresentation = arrRepresentation[1:len(arrRepresentation) - 2]
+        return [x.replace("\'", "") for x in arrRepresentation.split(',')]
+
+    def arrayDict(arr):
+        data = {}
+        header = []
+
+        for el in arr:
+            data[el] = []
+            header.append(el)
+            if el.find("\n") != -1:
+                break
+
+        for i in range(len(header), len(header)):
+            for j in range(len(header)):
+                data[header[j]] = arr[i + j]
+
+        return data
+
+
     out = [d for d in data.replace('SECURITY (auth/unicast/group)','SECURITY').split(' ') if d != '']
     print ' '.join(out)
-        
+    #print arrayDict(deSerialize(data))
+
 def doAction(device,opt,args):
-    if args and len(args) < 2:
+    def switchControl(options): 
+        return False if options.optScan and options.optConnect and options.optLogout else True 
+
+    if switchControl(opt):
 	if 'on' in args or 'off' in args:
             turn(args[0],device)
-	elif 'log' in args:
+	if opt.optConnect:
 		if exists(args[1]):
 			password = getpass.getpass("Password: ")
 			connect(args[1],device,password)
@@ -45,20 +71,19 @@ def doAction(device,opt,args):
 				print "Connection Success..."
 		else:
 			print "Network not found..."
-	elif 'logout' in args:
+        if opt.optLogout:
 	    subprocess.call("sudo airport -z", shell=True)
-	elif 'scan' in args:
+	if opt.optScan:
 		print "\t List of all wifi spots: "
 		betterPrint(subprocess.check_output("airport -s",shell=True)) #too long timing load before exec.
     else:
         print "Error input..."
+        sys.exit(32)
 
 if __name__ == "__main__":
-        (option,args) = getParser("usage: %prog [option] on off")
-	if args:
-                if len(args) > 2:
-                    print "Ups too many arguments"
-                    sys.exit(21)
-                doAction('en0',option,args)
+        (options,args) = getParser("usage: %prog [option] on off")
+        sys.exit
+	if args or options.optScan or options.optLogout or options.optConnect:
+                doAction('en0',options,args)
 	else:
             print "No input see the help"
