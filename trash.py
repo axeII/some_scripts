b5026e7e9a99f9802b273fb80d2d7bebb6b2f788
Ales Lerch <ales.lerch@gmail.com>
2016-09-05 01:09:33 +0200

    main,move,report imporved, setenviroemntc deleted,few func delted

--- trash.py
+++ trash.py
@@ -1,76 +1,63 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import os,sys
-from termcolor import colored
+import os
+import sys
 import shutil
+import optparse
+import os.path as op
 
-def setTrash():
-    try:
-        trash = os.environ['TRASH'] + '/'
-        return trash 
-    except KeyError:
-        print   "Local trash file is not set. Check your enviroment settings"
-        setEnviroment(raw_input('Do you want to set localTrash? [Y/N]'))
-        sys.exit(12)
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-trash = setTrash()
+try:
+    trash = os.environ['TRASH'] 
+except KeyError:
+    print "Local trash file is not set. Check your enviroment settings"
+    sys.exit(12)
 
-def setEnviroment(answer):
-    if answer.lower() in ('yes','y'):
-        with os.environ['HOME'] + "/.Trash" as directory:
-            if not os.path.isdir(directory):
-                os.makedirs(directory)
-            with open(os.environ['HOME']+'.bashrc','a') as locFile:
-                locFile.write('export TRASH=\"$HOME/.Trash\"')
+def report(argument,dir_option):
+    what = "Folder " if dir_option else "File   "
+    print "Info: " + bcolors.OKBLUE + what + bcolors.ENDC + bcolors.OKGREEN + argument + bcolors.ENDC
 
-def report(name):
-	if (os.path.isdir(trash+os.path.basename(name))):
-		what = "Folder"
-	else:
-		what = "File  "	 
-	print "\t%s\t%s" % (colored(what,'blue'),colored(name,'green'))
-
-def fileExists(what):
-	return os.path.exists(os.path.abspath(what))
-
-def testChar(char,name):
-	return char in name
+def reportError(argument):
+    print bcolors.FAIL + "Erro: " + bcolors.ENDC + bcolors.OKBLUE+ op.basename(argument) + bcolors.ENDC + bcolors.FAIL + " not found in this directory: " + bcolors.ENDC + bcolors.WARNING + op.dirname((op.abspath(argument))) + bcolors.ENDC
 
 def fixCase(which,what):
-	if which >= 1:
-		what = what[:-2]
-	return what	
+    return what[:-2] if which > 1 else what
 
-def move(from_,position,name):
-	if fileExists(from_):
-		if fileExists(name):
-			num = case = 0
-			while os.path.exists(position+name):
-				num += 1
-				if testChar('.',name):
-					end = name.split(".")[-1]
-					begin = ".".join(name.split(".")[:-1])
-					name = fixCase(case,begin) + "-" + str(num) + "." + str(end)
-				else:
-					name = fixCase(case,name) + "-" + str(num)
-				case += 1
-		shutil.move(from_,position+name)
-		return True
-	else:
-		return False
+def move(from_,position,name,dir_opt):
+    if op.exists(from_):
+        if op.exists(position + name):
+	    num_ = case = 0
+	    while op.exists(position + name):
+                case = case + 1
+		num_ = num_ + 1
+		if op.isfile:
+		    file_ = list(op.splitext(name))
+		    begin = ''.join(file_[:-1])
+		    name = fixCase(case,begin) + "-" + str(num_) + file_[-1]
+		else:
+		    name = fixCase(case,name) + "-" + str(num_)
+        report(op.abspath(name),dir_opt)		
+        shutil.move(from_,position+name)
+    else:
+        reportError(name)
 
 if __name__ == "__main__":
-	if len(sys.argv[1:]) > 0:
-		arguments = sys.argv[1:]
-		print "Moving to trash: \n"
-		for file_ in arguments:
-			original = os.path.abspath(file_)
-			status = move(os.path.abspath(file_),trash,os.path.basename(file_))
-			if status:
-				report(original)		
-			else:
-				print colored("\tError   %s not found in this directory: %s",'red') % (colored(os.path.basename(file_),'blue'),colored(os.path.dirname(os.path.abspath(file_)),'red'))
-
-	else:
-		 print "No input\nexample: trash /path/to/file.txt"
-
+    parser = optparse.OptionParser(usage="Usage %prog [option] file/s")
+    (options,args) = parser.parse_args()
+    if args:
+        print "Moving to trash: \n"
+        for arg in args:
+            file_name = op.basename(arg[:-1] if arg.endswith('/') else arg)
+            is_dir = True if op.isdir(arg) else False
+            move(op.abspath(arg),trash,file_name,is_dir)
+    else:
+        print "No input"
