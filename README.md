284896ba51d2c062ac97ab832d580e448269d38f
Ales Lerch <ales.lerch@gmail.com>
2016-09-18 14:37:24 +0200

    readme edited, namer.py added

--- README.md
+++ README.md
@@ -6,14 +6,40 @@ List of short scripts which could be use useful.
 
 These scripts were made in python and are made primary for os x. For easy use on os x all are made in python2.7 whih is primary python on os x. These scripts are often updated. Feel free to use any of them.
 
-### itun 
+### itun
 - script to moving and linking mp4 files
 - itun script for moving files to external disk to save local storage
 - after moving to external storage leave link to local storage
 
-### trash 
-- script to use to move any file to trash 
+### trash
+- script to use to move any file to trash
+Usage: Usage trash [option] file/s
 
-### wifi 
-- script to set wifi on os x with terminal 
-- no slow gui needed 
+Options:
+  -h, --help  show this help message and exit
+
+### wifi
+- script to set wifi on os x with terminal
+- no need to use default os x long commands to turn wifi on
+- Usage: wifi [option] on off
+
+Options:
+  -h, --help            show this help message and exit
+  -c OPTCONNECT, --connect=OPTCONNECT
+                        Which wifi you want to connect
+  -l, --logout          logout from wifi network
+  -s, --scan            scan local network area
+  -i                    List hardware info
+
+### namer
+- script for namer files
+Usage: namer [options]
+
+Options:
+  -h, --help  show this help message and exit
+  -r          Read number file by regex
+  -p          Rename all files by current directory
+  -d          Rename directory as well
+  -f FILEN    Exact name you want each file name
+  -n          Dry print just print all action not execute
+  -c          acts like mv
---
+++ scripts/namer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import optparse
+import sys,os
+import re,itertools
+from random import randint
+from os.path import *
+
+def parseArgs():
+	parser = optparse.OptionParser()
+	parser.add_option('-r',dest='rex',help='Read number file by regex',action='store_true')
+        parser.add_option('-p',dest='dirN',help='Rename all files by current directory',action='store_true')
+        parser.add_option('-d',dest='sDir',help='Rename directory as well',action='store_true')
+        parser.add_option('-f',dest='fileN',help='Exact name you want each file name')
+        parser.add_option('-n',dest='noExe',help='Dry print just print all action not execute',action='store_true')
+        parser.add_option('-c',dest='clean',help='acts like mv',action='store_true')
+	return parser.parse_args()
+
+def filterArgs(args,options):
+    field,paths = (list(),map(lambda x:dirname(realpath(x)),args)) 
+    [field.append(os.listdir(a) if isdir(a) and not options.sDir else [a]) for a in args]
+    return (list(itertools.chain.from_iterable(field)),(realpath(args[0])) if all(x==paths[0] for x in paths) else sys.exit('Some paths are diferent'),options)
+
+def fixName(myPath,num,inp):
+    return "%s/%s%s" % (myPath if isdir(myPath) else dirname(myPath),'{0:03d}'.format(num),splitext(inp)[1])
+
+def nameThoseFiles(files,tPath,option):
+    if option.dirN and option.fileN:
+        sys.exit("Error too many switches")
+    for i in range(len(files)):
+        if files[i].startswith('.'): continue
+        if option.dirN or option.fileN:
+            if exists(join(dirname(tPath),option.fileN)): sys.exit('Ups wrong file name!')
+            new ="%s/%s_%s%s" % (dirname(tPath),basename(tPath) if option.dirN else option.fileN,'{0:0>3}'.format(i),splitext(files[i])[1]) 
+        elif option.rex:
+            try:
+                newIndex = int(re.findall(r'\d+',files[i])[-1])
+            except IndexError:
+                newIndex = 0
+            new = fixName(tPath,newIndex,files[i]) 
+        else:
+            new = fixName(tPath,i,files[i]) 
+        if option.noExe:
+            print "renaming from %s to %s"%(files[i],basename(new))
+        else: 
+            try:
+                os.rename(join(dirname(tPath) if not isdir(tPath) else tPath,files[i]),new)
+            except Exception as e:
+                print 'Ups wrong naming: ',e
+
+if __name__ == "__main__":
+    (options,args) = parseArgs()
+    #[nameThoseFiles([arg] if options.sDir else os.listdir(arg) if isdir(arg) else [arg],dirname(realpath(arg)),options) for arg in args] if args else sys.exit("No input")
+    nameThoseFiles(*filterArgs(args,options)) if args else sys.exit('No inputs given...')
