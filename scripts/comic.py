649692bb1e80e993602a2d6ef33d022c6a8d63ec
Ales Lerch <ales.lerch@gmail.com>
2017-08-11 00:40:41 +0200

    update comic script

--- scripts/comic.py
+++ scripts/comic.py
@@ -1,132 +1,108 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
 
 import os,sys
+import pdb
 import zipfile
 import optparse
-import magic
+try:
+    import magic
+except:
+    print("[Warning] No magic library inputed some ferature not woking properly")
 from subprocess import call
 
-images = ("jpg","jpeg","png","tiff","gif","bmp")
-archives = ('cbr','cbz','cb7','zip')
-incompatible = []
+class Comic:
 
-def testFile(file_,folder):
-	return os.path.isfile(os.path.join(folder,file_)) and file_.split('.')[-1].lower() in images
+    def __init__(self):
+        self.images = ("jpg","jpeg","png","tiff","gif","bmp")
+        self.archives = ('cbr','cbz','cb7','zip')
+        self.incompatible = set()#.DS_store eg
 
-def recognizeFile(file_):
-    type_ = magic.from_file(file_,mime=True).replace('image/','')
-    if not type_ in ('jpeg','png'):
-        return None
-    return type_ if type_ else file_.split('.')[-1]
+        parser = optparse.OptionParser()
+        parser.add_option('-n','--name',dest='name',help='Set name to name for inputed file')
+        parser.add_option('-d','--dir',dest='target_path',help='Set path for directory where to save file',default='./')
+        parser.add_option('-e','--extention',dest='extention',help='Set which exetentsion should be converted', default="cbz")#nargs='*'
+        #parser.add_option('-t',dest='trash',help='Trash file after convertion',action='store_true',default=False)
+        parser.add_option('-c',dest='clean',help='File name without empty characters',action='store_true',default=False)
+        parser.add_option('-v',dest='verbose', help='Verbose output', action='store_true', default=False)
+        (self.options, self.args) = parser.parse_args()
+        self.reset_settings()
 
-def serachFile(directory,dictionary,num_):
-	stream = os.listdir(directory)
-	for input_ in stream:
-		testDir = os.path.join(os.path.abspath(directory),input_)
-		if os.path.isdir(testDir):
-			 dictionary,num_ = serachFile(testDir,dictionary,num_)
-		else:
-			if testFile(input_,directory):
-                                os.chmod(os.path.join(directory,input_),436)
-				num_ += 1
-				type_ = recognizeFile(os.path.join(directory,input_)) 
-                                pole = [input_,os.path.expandvars(os.path.realpath(directory)),type_]
-				dictionary[num_] = pole
-			else:	
-				if input_ not in incompatible: incompatible.append(input_)
-	return (dictionary,num_) 
+        if self.options.extention not in self.archives:
+            sys.exit("Not supported extension")
 
-def printFile(what):
-	sys.stdout.write(what)
-	sys.stdout.flush()
+        if self.args:
+            for arg in self.args:
+                self.search_file(arg)
+                self.zip_archive(self.fix_input_n(arg))
+                self.reset_settings()
+                print("\nNot compatible files:{}".format(','.join([inc for inc in self.incompatible])))
+            #call("trash " + os.path.abspath(trasher.replace(' ','\ ').replace(')','\)').replace('(','\(')),shell=True) 
+        else:
+            parser.print_help()
 
-def shorte(word):
-	if len(word) > 7:
-		word = "_" + word[-7:]
-	return word
+    def fix_input_n(self,input_file):
+        if not self.options.name:
+            def_name = input_file[:-1] if input_file.endswith('/') else input_file
+            return ''.join(def_name.split('/')[-1])
+        return self.options.name
 
-def clearName(name,control):
-	return name.replace(" ",'_') if control else name
+    def reset_settings(self):
+        self.number = 0
+        self.index = 1
+        self.dict = {}
 
-def resultName(default_name,optional):
-    if default_name.endswith('/'): default_name = default_name[:-1]
-    default_name = ''.join(default_name.split('/')[-1])
-    return optional if optional else default_name
+    def recognize_file(self,file_):
+        try:
+            return magic.from_file(file_,mime=True).replace('image/','')
+        except:
+            return file_.split('.')[-1].lower()
 
-def zipArchive(name,dictionary,path_,isZipfile,i,runIt):
-    if runIt:
-	if dictionary: 
-		f = zipfile.ZipFile(path_ + clearName(name,isZipfile) + '.' + typeControl(isZipfile),'w')
-		for dic in dictionary:
-			printFile("\rAdding %8s ( %s of %s) to %s" % (shorte(dictionary[dic][0]),'{0:0>3}'.format(i),len(dictionary),name))
-			f.write('%s/%s' % (dictionary[dic][1],dictionary[dic][0]),'%s/%s%s.%s' % (name,name,'_{0:0>3}'.format(dic),dictionary[dic][2]))
-			i += 1
-		print ""
-		f.close()
+    def search_file(self,if_name):
 
-def testPath(path_):
-    return './' if not path_ else str(path_) + '/'
+        def test_file(file_,folder):
+            return os.path.isfile(os.path.join(folder,file_)) and self.recognize_file(file_) in self.images
 
-def extract(files,sourcePath,destionPath):
-	for file_ in files:
-		if file_[-3:] == 'cbr':
-			zip_ref = zipfile.ZipFile(sourcePath + file_, 'r')
-			zip_ref.extractall(destionPath)
-			zip_ref.close()
+        for input_ in os.listdir(if_name):
+            testDir = os.path.join(os.path.abspath(if_name),input_)
+            if os.path.isdir(testDir):
+                search_file(testDir,self.dict)
+            else:
+                if test_file(input_,if_name):
+                    os.chmod(os.path.join(if_name,input_),436)
+                    self.number += 1
+                    type_ = self.recognize_file(os.path.join(if_name,input_))
+                    self.dict[self.number] = [input_, os.path.expandvars(os.path.realpath(if_name)) if self.options.target_path == "./" else self.options.target_path, type_]
+                else:
+                    if input_ not in self.incompatible:
+                        self.incompatible.add(input_)
 
-def typeControl(status):
-	return 'zip' if status else 'cbr'
-		
-def zipController(folder_,opt_name,fileDic_,path_,zipIt,zipOnly):
-    name_file = resultName(folder_,opt_name)
-    zipArchive(name_file,fileDic_,testPath(path_),zipIt,1,zipIt)
-    zipArchive(name_file,fileDic_,testPath(path_),False,1,not zipOnly)
+    def zip_archive(self,file_name):
 
-def comicMode():
-	print "Entering comic mode..."
-	what = True
-	volume = 0
-	print "Current Volume is " + str(volume) + " what is your order?"
-	while what:
-		order = raw_input('Next[N],Previos[P],Status[S],Reset[R],End[E]: ')
-		if order == "N" or order == "n":
-			volume += 1
-		        call("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume),shell=True)
-		elif order == "P" or order == "p":
-			volume -= 1
-			call("open -a DrawnStrips\ Reader ./*%s.cbr" % (volume),shell=True)
-		elif order == "S" or order == "s":
-			print "Your current volume is " + str(volume)
-			call("ls -lh",shell=True)
-		elif order == "E" or order == "e":
-			what = False
-                        print "Goodbye..."
-		elif order == "R" or order == "r":
-			print "Reseting from %s to 0" % (volume)
-			volume = 0
-		else:
-			print "Wrong input.."
+        def flush_print(what):
+            sys.stdout.write(what)
+            sys.stdout.flush()
+
+        def shorte(word):
+            return word if len(word) < 7 else "_{}".format(word[-7:])
+
+        def clean_name(name):
+            return name.replace(" ",'_') if self.options.clean else name
+
+        path = self.options.target_path if self.options.target_path.endswith('/') else self.options.target_path + '/'
+        if self.dict:
+            comp_f = zipfile.ZipFile("{}{}.{}".format(path,
+                clean_name(file_name),self.options.extention),'w')
+            for dic in self.dict:
+                flush_print("\rAdding %8s ( %s of %s) to %s" %
+                        (shorte(self.dict[dic][0]),'{0:0>3}'.format(self.index),len(self.dict),file_name))
+                comp_f.write('{}/{}'.format(self.dict[dic][1],self.dict[dic][0]),
+                        '{}/{}{}.{}'.format(file_name,file_name,'_{0:0>3}'.format(dic),self.dict[dic][2]))
+                if self.options.verbose:
+                    print('{}/{}'.format(self.dict[dic][1],self.dict[dic][0]),
+                            '{}/{}{}.{}'.format(file_name,file_name,'_{0:0>3}'.format(dic),self.dict[dic][2]))
+
+                self.index += 1
+            comp_f.close()
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option('-d','--dir',dest='path',help='save to where')
-    parser.add_option('-z',dest='isZip',help='zipit',action='store_true',default=False)
-    parser.add_option('-m',dest='mode',help='start the mode',action='store_true',default=False)
-    parser.add_option('-n','--name',dest='name',help='set file name')
-    parser.add_option('-t',dest='trash',help='Do you want to trash file after compresion?',action='store_true',default=False)
-    parser.add_option('-o',dest='zipOnly',help='zip only',action='store_true',default=False)
-    (options,args),trashers = (parser.parse_args(),list())
-    if options.mode: comicMode()
-    if args:
-        for folder in args:
-            global_,fileDic = (0,dict())
-            fileDic,global_ = serachFile(folder,fileDic,global_)
-            zipController(folder,options.name,fileDic,options.path,options.isZip,options.zipOnly)
-            if options.trash:
-                trashers.append(folder)
-        if trashers:
-            for trasher in trashers:
-                call("trash " + os.path.abspath(trasher.replace(' ','\ ').replace(')','\)').replace('(','\(')),shell=True) 
-        print "Not compatible files:",','.join([comp for comp in incompatible])
-    else:
-        print "usage:  comic mode \n\tcomic -m zip file | comic -d path"
+    comic = Comic()
